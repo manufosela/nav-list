a0ae97ae4a2e005b9f708e3c8d2ea2177e48d1cb
Manuel Juan Fosela Águila <manufosela@gmail.com>
2019-04-16 19:24:49 +0200

    updated to lit-element 2.1.0

--- nav-list.js
+++ nav-list.js
@@ -1,11 +1,13 @@
-import { LitElement, html } from '/node_modules/@polymer/lit-element/lit-element.js';
-import {afterNextRender, beforeNextRender} from '/node_modules/@polymer/polymer/lib/utils/render-status.js';
+import { LitElement, html, css } from '/node_modules/lit-element/lit-element.js';
+//import {afterNextRender, beforeNextRender} from '/node_modules/lib/utils/render-status.js';
 
 /**
  * `nav-list`
- * 
+ * Nav List
  *
  * @customElement
+ * @polymer
+ * @litElement
  * @demo demo/index.html
  */
 class NavList extends LitElement {
@@ -19,22 +21,81 @@ class NavList extends LitElement {
     }
   }
 
+  static get styles(){
+    return css`
+      @media screen and (max-width: 767px) {
+        .navlist-labels {
+          width: 100%;
+        }
+      }
+      .navlist-labels__checkbox {
+        display: none;
+      }
+      .navlist-labels__checkbox:checked + .navlist-labels__txt {
+        border-color: #cc3743;
+        padding: 10px 13px;
+      }
+      .navlist-labels__title {
+        font-family: "Dosis", sans-serif;
+        font-weight: 700;
+        letter-spacing: 3px;
+        font-size: 16px;
+        margin-bottom: 10px;
+      }
+      .navlist-labels__group {
+        display: flex;
+        margin-bottom: 15px;
+      }
+      @media screen and (max-width: 992px) {
+        .navlist-labels__group {
+          justify-content: center;
+        }
+      }
+      .navlist-labels__group:last-child {
+        margin-bottom: 0;
+      }
+      .navlist-labels__item {
+        margin: 5px;
+        cursor: pointer;
+      }
+      .navlist-labels__item:first-child {
+        margin-left: 0;
+      }
+      .navlist-labels__txt {
+        display: block;
+        border: 2px solid transparent;
+        font-size: 14px;
+        padding: 10px 20px;
+        padding-left: 0;
+        border-radius: 50px;
+        transition: all 0.3s;
+        letter-spacing: 2px;
+      }
+    `;
+  }
+
   constructor() {
     super();
-    beforeNextRender(this, function() {
-      this.listValues = this.list.split(",");
-    });
-    afterNextRender(this, function() {
-      if (this.value) {
-        this.renderRoot.querySelector("[id='navlist-item__"+this.value+"']").checked = true;
-      }
-    });
-  
     this.list = "0,1,2,3,4,5";
     this.listValues = this.list.split(",");
     this.value = null;
     this.title='TITLE';
     this.fixed = false;
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.listValues = this.list.split(",");
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+  }
+
+  updated() {
+    if (this.value) {
+      this.renderRoot.querySelector("[id='navlist-item__"+this.value+"']").checked = true;
+    }
   }
 
   _setValue(val) {
@@ -56,56 +117,6 @@ class NavList extends LitElement {
 
   render() {
     return html`
-      <style>
-        @media screen and (max-width: 767px) {
-          .navlist-labels {
-            width: 100%;
-          }
-        }
-        .navlist-labels__checkbox {
-          display: none;
-        }
-        .navlist-labels__checkbox:checked + .navlist-labels__txt {
-          border-color: #cc3743;
-          padding: 10px 13px;
-        }
-        .navlist-labels__title {
-          font-family: "Dosis", sans-serif;
-          font-weight: 700;
-          letter-spacing: 3px;
-          font-size: 16px;
-          margin-bottom: 10px;
-        }
-        .navlist-labels__group {
-          display: flex;
-          margin-bottom: 15px;
-        }
-        @media screen and (max-width: 992px) {
-          .navlist-labels__group {
-            justify-content: center;
-          }
-        }
-        .navlist-labels__group:last-child {
-          margin-bottom: 0;
-        }
-        .navlist-labels__item {
-          margin: 5px;
-          cursor: pointer;
-        }
-        .navlist-labels__item:first-child {
-          margin-left: 0;
-        }
-        .navlist-labels__txt {
-          display: block;
-          border: 2px solid transparent;
-          font-size: 14px;
-          padding: 10px 20px;
-          padding-left: 0;
-          border-radius: 50px;
-          transition: all 0.3s;
-          letter-spacing: 2px;
-        }
-      </style>
       <div class="navlist-labels">
         <div class="navlist-labels__title">${this.title}</div>
         <div class="navlist-labels__group">
